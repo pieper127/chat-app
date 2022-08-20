6f668f2ada92ef3b1c27097771888aa445862596
Stijn <stijnbijnen@hotmail.com>
2022-08-20 17:22:25 +0300

    Add client so that the server can be tested

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -2,7 +2,14 @@ import express, { Application, Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser";
 import Connect from "./connect-db";
 import apiRouter from "./router";
+import http from "http";
+import { Server } from "socket.io";
+import authenticateJWT, { authenticateJWTForSockIo } from "./middleware/jwt";
+
 const app: Application = express();
+const server = http.createServer(app);
+const io = new Server(server)
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -13,6 +20,12 @@ const db = "mongodb://root:root@localhost:27017/";
 
 Connect({ db });
 
-app.listen(PORT, () => {
+io.use(authenticateJWTForSockIo);
+
+io.on('connection', (socket) => {
+  socket.on("message-to-room", (_arg) => console.log("here"));
+})
+
+server.listen(PORT, () => {
   console.log(`server is running on PORT ${PORT}`);
 })
--- back-end/src/middleware/jwt.ts
+++ back-end/src/middleware/jwt.ts
@@ -1,22 +1,58 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
+import { Socket } from "socket.io";
 
 const JWT_KEY = process.env.JWT_SECRET || "123456";
 
 function authenticateJWT(req: Request, res: Response, next: NextFunction) {
     const authHeader = req.headers.authorization;
-    if (authHeader && authHeader !== "null") {
-        jwt.verify(authHeader, JWT_KEY, (err: any, user: any) => {
-            if (err) {
-                return res
-                    .status(403)
-                    .send({ success: false, message: "Token Expired" });
-            }
-            (req as any).user = user;
-            next();
-        })
+    const valid = checkValidity(authHeader);
+
+    if (valid.ok) {
+        const payload = jwt.decode(authHeader as string);
+        (req as any).userEmail = (payload as any).email;
+        next();
     } else {
-        res.status(403).json({ success: false, message: "UnAuthorized" });
+        res
+            .status(403)
+            .send({ success: false, message: valid.err });
     }
 }
+
+// type is defined because socket.io doesn't export this type
+type ExtendedError = any;
+export function authenticateJWTForSockIo(socket: Socket, next: (err?: ExtendedError | undefined) => void) {
+    const authHeader = socket.handshake.auth.token;
+    const valid = checkValidity(authHeader);
+
+    if (valid.ok) {
+        next();
+    } else {
+        next(new Error(valid.err));
+    }
+}
+
+interface Error<T> {
+    ok: false;
+    err: T;
+}
+interface Ok {
+    ok: true;
+}
+type Result<T> = Error<T> | Ok;
+function checkValidity(token: string | undefined): Result<string> {
+    if (token && token !== "null") {
+        const validatedToken = jwt.verify(token, JWT_KEY);
+
+        if (!validatedToken) {
+            return {
+                ok: false,
+                err: "Token Expired"
+            };
+        }
+        return { ok: true };
+    } 
+    return { ok: false, err: "UnAuthorized" };
+}
+
 export default authenticateJWT;
